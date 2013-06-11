a5d2135c3f81a30fe17a7141d48feea4372b3319
valdergallo <valdergallo@gmail.com>
2013-06-11 10:42:30 -0300

    add validate to form fields

--- django_choices_flow/forms.py
+++ django_choices_flow/forms.py
@@ -8,29 +8,33 @@ from django.core import exceptions
 
 class FlowCharField(forms.CharField):
 
+    @staticmethod
+    def get_db_value(model_instance):
+        if model_instance.id:
+            return model_instance.__class__.objects.get(id=model_instance.id)
+
     def validate(self, value, model_instance):
-        """
-        Validates value and throws ValidationError. Subclasses should override
-        this to provide validation logic.
-        """
-        super(FlowCharField, self).validate(value, model_instance)
         # validate choice flow
-        if self.choices.validate(getattr(model_instance, self.name), value):
-            return
-        else:
-            raise exceptions.ValidationError(_('%s Invalid Choice Flow' % value))
+        if model_instance.id:
+            db_value = self.get_db_value(model_instance)
+            if not self.choices.validate(getattr(db_value, self.name), value):
+                raise exceptions.ValidationError(_('%s is a invalid choice in this flow' % value))
+
+        super(FlowIntegerField, self).validate(value, model_instance)
 
 
 class FlowIntegerField(forms.IntegerField):
 
+    @staticmethod
+    def get_db_value(model_instance):
+        if model_instance.id:
+            return model_instance.__class__.objects.get(id=model_instance.id)
+
     def validate(self, value, model_instance):
-        """
-        Validates value and throws ValidationError. Subclasses should override
-        this to provide validation logic.
-        """
-        super(FlowIntegerField, self).validate(value, model_instance)
         # validate choice flow
-        if self.choices.validate(getattr(model_instance, self.name), value):
-            return
-        else:
-            raise exceptions.ValidationError(_('%s Invalid Choice Flow' % value))
+        if model_instance.id:
+            db_value = self.get_db_value(model_instance)
+            if not self.choices.validate(getattr(db_value, self.name), value):
+                raise exceptions.ValidationError(_('%s is a invalid choice in this flow' % value))
+
+        super(FlowIntegerField, self).validate(value, model_instance)
