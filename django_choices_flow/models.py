1febfa7e4c5a4243396f1a9068f9405f66cd3ed7
Valder Gallo <valdergallo@gmail.com>
2013-06-11 23:09:08 -0300

    fixed models

--- django_choices_flow/models.py
+++ django_choices_flow/models.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-
 from django.core import exceptions
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
 class FlowCharField(models.CharField):
-
-    def __init__(self, *args, **kwargs):
-        super(FlowCharField, self).__init__(*args, **kwargs)
-        if not self.max_length:
-            self.max_length = max([i[0] for i in self.choices])
 
     @staticmethod
     def get_db_value(model_instance):
@@ -27,12 +20,10 @@ class FlowCharField(models.CharField):
             if not self.choices.validate(getattr(db_value, self.name), value):
                 raise exceptions.ValidationError(_('%s is a invalid choice in this flow' % value))
 
-        super(FlowIntegerField, self).validate(value, model_instance)
+        super(FlowCharField, self).validate(value, model_instance)
 
 
 class FlowIntegerField(models.IntegerField):
-    def __init__(self, *args, **kwargs):
-        super(FlowIntegerField, self).__init__(*args, **kwargs)
 
     @staticmethod
     def get_db_value(model_instance):
